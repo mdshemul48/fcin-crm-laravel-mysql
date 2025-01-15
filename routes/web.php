13e8b1a55abfe6cdf3d5bd846785e6b83ead3515
MD. Shimul <mdshemul480@gmail.com>
2025-01-16 00:30:12 +0600

    fixing routing

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,18 @@
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view("dashboard");
-})->middleware("auth")->name("dashboard");
+Route::middleware('guest')->group(function () {
+    Route::get('login', [AuthenticatedSessionController::class, 'showLoginForm'])->name('login');
+    Route::post('login', [AuthenticatedSessionController::class, 'store']);
+});
 
-Route::get('login', [AuthenticatedSessionController::class, 'showLoginForm'])->name('login');
-Route::post('login', [AuthenticatedSessionController::class, 'store']);
-Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+Route::middleware('auth')->group(function () {
+    Route::get('/', function () {
+        return view("dashboard");
+    })->name('dashboard');
+
+    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+});
 
 
 
