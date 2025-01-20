1d53e9969d4a78c6e7ce9dbc2ee82291cbe66313
MD. Shimul <mdshemul480@gmail.com>
2025-01-21 00:02:47 +0600

    fixing nab bar

--- resources/views/components/navigation-menu.blade.php
+++ resources/views/components/navigation-menu.blade.php
@@ -1,5 +1,12 @@
-<li class="nav-item">
-    <a class="nav-link" href="{{ route('clients.index') }}">Clients</a>
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        Clients Management
+    </a>
+    <ul class="dropdown-menu">
+        <li> <a class="nav-link" href="{{ route('clients.index') }}">Clients</a>
+        </li>
+        <li> <a class="nav-link" href="{{ route('packages.index') }}">Packages</a>
+    </ul>
 </li>
 <li class="nav-item">
     <a class="nav-link" href="#">Sub Reseller</a>
